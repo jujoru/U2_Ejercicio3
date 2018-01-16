e79a67ec2d3ad2d9f6c3ba55e55f0a195b3420ca
jujoru86 <jujoru86@gmail.com>
2018-01-16 16:30:10 +0100

    NINO: Subida cambios 1

---
+++ app/src/main/java/es/jujoru/u2_ejercicio3/AdaptadorCoches.java
@@ -0,0 +1,8 @@
+package es.jujoru.u2_ejercicio3;
+
+/**
+ * Created by Nino Ruano on 16/01/2018.
+ */
+
+public class AdaptadorCoches {
+}
---
+++ app/src/main/java/es/jujoru/u2_ejercicio3/Coche.java
@@ -0,0 +1,51 @@
+package es.jujoru.u2_ejercicio3;
+
+/**
+ * Created by Nino Ruano on 16/01/2018.
+ */
+
+public class Coche {
+     String marca;
+     String modelo;
+     String imagen;
+     int precio;
+
+    public Coche(String marca, String modelo, String imagen, int precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.imagen = imagen;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+}
