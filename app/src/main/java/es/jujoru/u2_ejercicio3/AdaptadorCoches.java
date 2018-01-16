e5cfd7836900b5520df15282c935f019c7614baa
jujoru86 <jujoru86@gmail.com>
2018-01-16 16:51:26 +0100

    NINO: Subida cambios 2

--- app/src/main/java/es/jujoru/u2_ejercicio3/AdaptadorCoches.java
+++ app/src/main/java/es/jujoru/u2_ejercicio3/AdaptadorCoches.java
@@ -1,8 +1,47 @@
 package es.jujoru.u2_ejercicio3;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Nino Ruano on 16/01/2018.
  */
 
-public class AdaptadorCoches {
+public class AdaptadorCoches extends ArrayAdapter<Coche> {
+
+    ArrayList<Coche> coches;
+    Context c;
+
+    public AdaptadorCoches(Context c, ArrayList<Coche> coches) {
+        super(c, R.layout.item_coche, coches);
+        this.coches = coches;
+        this.c = c;
+    }
+
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        View item = inflater.inflate(R.layout.item_coche, null);
+
+        TextView tv_marca = (TextView) item.findViewById(R.id.item_marca);
+        tv_marca.setText(coches.get(position).getMarca());
+
+        TextView tv_modelo = (TextView) item.findViewById(R.id.item_modelo);
+        tv_modelo.setText(coches.get(position).getModelo());
+
+        String logo = coches.get(position).getImagen();
+
+        int idImagen = c.getResources().getIdentifier(logo, "drawable", c.getPackageName());
+        ImageView iv_logo = (ImageView) item.findViewById(R.id.item_logo);
+        iv_logo.setImageResource(idImagen);
+
+        return item;
+    }
+
 }
--- app/src/main/java/es/jujoru/u2_ejercicio3/MainActivity.java
+++ app/src/main/java/es/jujoru/u2_ejercicio3/MainActivity.java
@@ -2,12 +2,47 @@ package es.jujoru.u2_ejercicio3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv_coches;
+    ArrayList<Coche> lista_coches = new ArrayList<Coche>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarDatos();
+
+        lv_coches = (ListView)findViewById(R.id.lv_coches);
+        AdaptadorCoches adaptadorCoches=new AdaptadorCoches(this,
+                lista_coches);
+
+        lv_coches.setAdapter(adaptadorCoches);
+        lv_coches.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Coche c =((Coche)parent.getItemAtPosition(position));
+                int precio = c.getPrecio();
+            }
+        });
+
+    }
+
+    private void cargarDatos(){
+         lista_coches.add(new Coche("Seat","Alhambra","alhambra",13000));
+        lista_coches.add(new Coche("Seat","Altea","altea",17500));
+        lista_coches.add(new Coche("Seat","Arona","arona",12300));
+        lista_coches.add(new Coche("Seat","Ateca","ateca",21500));
+        lista_coches.add(new Coche("Seat","Bocanegra","bocanegra",19000));
+        lista_coches.add(new Coche("Seat","Cupra","cupra",18500));
+        lista_coches.add(new Coche("Seat","Leon","leon",14900));
+        lista_coches.add(new Coche("Seat","Ibiza","ibiza",9000));
     }
 }
