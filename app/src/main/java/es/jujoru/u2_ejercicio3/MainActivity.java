8b643c796b0a77e96da038fc54bf6a02386faaa5
jujoru86 <jujoru86@gmail.com>
2018-01-16 16:28:14 +0100

    Initial commit

---
+++ app/src/main/java/es/jujoru/u2_ejercicio3/MainActivity.java
@@ -0,0 +1,13 @@
+package es.jujoru.u2_ejercicio3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
